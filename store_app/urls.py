ea90af800e8cd94560067862e0910581bf0edf58
Niha Ahmad <nihaahmad26@gmail.com>
2022-04-03 11:44:26 -0500

    added views and urls

---
+++ store_app/urls.py
@@ -0,0 +1,12 @@
+from unicodedata import name
+from django.urls import path
+
+from . import views
+
+app_name = 'store'
+
+urlpatterns = [
+    path('',views.all_products, name='all_products'),
+    path('item/<slug:slug>/', views.product_detail, name='product_detail'),
+    
+]
--- store_app/views.py
+++ store_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Category, Product
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+def product_detail(request,slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store/products/detail.html', {'product': product})
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
+
