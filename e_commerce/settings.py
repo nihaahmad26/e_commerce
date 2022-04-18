9779c5bf94e0c2c5b4a758eba30a19f99a4344ef
Niha Ahmad <nihaahmad26@gmail.com>
2022-04-18 14:57:15 -0500

    commit on 'refactoring'

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'store_app.views.categories',
+                'store_app.context_processors.categories',
             ],
         },
     },
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- store_app/views.py
+++ store_app/views.py
@@ -3,16 +3,12 @@ from django.shortcuts import get_object_or_404
 
 from .models import Category, Product
 
-def categories(request):
-    return {
-        'categories': Category.objects.all()
-    }
 
 def product_detail(request,slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/detail.html', {'product': product})
+    return render(request, 'store/products/single.html', {'product': product})
 
-def all_products(request):
+def product_all(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
 
